4c416d5af64e6b3606bd358cf4f27106805857bc
MoNRa <ju4nrsuarez@gmail.com>
2019-04-21 01:09:20 -0300

    primer commit

---
+++ precache-manifest.c3397772a59a6ce689e8b1b11ac3c899.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "4cf659aeea4f957c0726",
+    "url": "/static/js/main.97857991.chunk.js"
+  },
+  {
+    "revision": "c919152e9594a3e1104d",
+    "url": "/static/js/2.0a205d6d.chunk.js"
+  },
+  {
+    "revision": "4cf659aeea4f957c0726",
+    "url": "/static/css/main.adeb4f8f.chunk.css"
+  },
+  {
+    "revision": "654972034aa6505074866191390a8e1c",
+    "url": "/index.html"
+  }
+];
